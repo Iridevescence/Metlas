ae793e60bba00e0293ac60ab6593e17dde933931
PrismaticYT <45874270+RPMYT@users.noreply.github.com>
2022-11-16 23:29:37 +0000

    Add project skeleton

---
+++ src/main/java/com/iridevescence/metlas/Metlas.java
@@ -0,0 +1,10 @@
+package com.iridevescence.metlas;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Metlas implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        
+    }
+}
